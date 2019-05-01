8621d8034b4f5d17e148171a0f9e220269fd8d91
unknown <iosoloiowow@gmail.com>
2019-05-01 12:00:19 +0200

    A001 Fertig A002 Angefangen

---
+++ GUI/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartTxt = new System.Windows.Forms.TextBox();
+            this.ArrivalTxt = new System.Windows.Forms.TextBox();
+            this.StartLstBox = new System.Windows.Forms.ListBox();
+            this.ArrivalLstBox = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.Connections = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // StartTxt
+            // 
+            this.StartTxt.Location = new System.Drawing.Point(6, 19);
+            this.StartTxt.Name = "StartTxt";
+            this.StartTxt.Size = new System.Drawing.Size(100, 20);
+            this.StartTxt.TabIndex = 0;
+            // 
+            // ArrivalTxt
+            // 
+            this.ArrivalTxt.Location = new System.Drawing.Point(184, 19);
+            this.ArrivalTxt.Name = "ArrivalTxt";
+            this.ArrivalTxt.Size = new System.Drawing.Size(100, 20);
+            this.ArrivalTxt.TabIndex = 1;
+            // 
+            // StartLstBox
+            // 
+            this.StartLstBox.FormattingEnabled = true;
+            this.StartLstBox.Location = new System.Drawing.Point(141, 97);
+            this.StartLstBox.Name = "StartLstBox";
+            this.StartLstBox.Size = new System.Drawing.Size(178, 121);
+            this.StartLstBox.TabIndex = 2;
+            // 
+            // ArrivalLstBox
+            // 
+            this.ArrivalLstBox.FormattingEnabled = true;
+            this.ArrivalLstBox.Location = new System.Drawing.Point(325, 97);
+            this.ArrivalLstBox.Name = "ArrivalLstBox";
+            this.ArrivalLstBox.Size = new System.Drawing.Size(174, 121);
+            this.ArrivalLstBox.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(531, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OnSearchClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(531, 126);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(141, 250);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(567, 147);
+            this.listBox3.TabIndex = 6;
+            // 
+            // Connections
+            // 
+            this.Connections.AutoSize = true;
+            this.Connections.Location = new System.Drawing.Point(138, 234);
+            this.Connections.Name = "Connections";
+            this.Connections.Size = new System.Drawing.Size(73, 13);
+            this.Connections.TabIndex = 7;
+            this.Connections.Text = "Verbindungen";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(144, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Startstation";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(322, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Zielstation";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ArrivalTxt);
+            this.groupBox1.Controls.Add(this.StartTxt);
+            this.groupBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.groupBox1.Location = new System.Drawing.Point(141, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(358, 68);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Connections);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ArrivalLstBox);
+            this.Controls.Add(this.StartLstBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox StartTxt;
+        private System.Windows.Forms.TextBox ArrivalTxt;
+        private System.Windows.Forms.ListBox StartLstBox;
+        private System.Windows.Forms.ListBox ArrivalLstBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label Connections;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ GUI/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SwissTransport;
+
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonCLick(object sender, EventArgs e)
+        {
+            Transport t = new Transport();
+
+            Stations mystations = t.GetStations("Luz");
+        }
+
+        private void OnSearchClick(object sender, EventArgs e)
+        {
+            foreach (TextBox s in groupBox1.Controls)
+            {
+                string TxtBoxName = s.Name;
+                GetListStations(TxtBoxName);
+            }
+        }
+        public void GetListStations(string TxtBoxName)
+        {
+            Transport T = new Transport();
+            Stations S = T.GetStations(TxtBoxName);
+            List<string> StartList = new List<string>();
+            foreach (Station s in S.StationList)
+            {
+                StartList.Add(s.Name);
+            }
+            StartLstBox.DataSource = null;
+            StartLstBox.DataSource = StartList;
+            
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ArrivalLstBox.Items.Add("Um Verbindungen zu suchen");
+            ArrivalLstBox.Items.Add("Ziel Text Box ausfüllen");
+        }
+    }
+}
